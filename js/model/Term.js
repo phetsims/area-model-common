66628a72dab80c98a81af5c15c16a9c269fa1feb
Jonathan Olson <olsonsjc@gmail.com>
2017-05-09 23:06:28 -0600

    Restructuring, adding Term.js

---
+++ js/model/Term.js
@@ -0,0 +1,41 @@
+// Copyright 2017, University of Colorado Boulder
+
+/**
+ * A single mathematical term (the product of a coefficient with a power of x).
+ *
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var areaModelCommon = require( 'AREA_MODEL_COMMON/areaModelCommon' );
+
+  /**
+   * @constructor
+   *
+   * @param {number} coefficient
+   * @param {number} [power]
+   */
+  function Term( coefficient, power ) {
+    // Allow only specifying one argument to the function (the coefficient).
+    power = ( power === undefined ? 0 : power );
+
+    assert && assert( typeof coefficient === 'number' && isFinite( coefficient ),
+      'Coefficient only needs to be a finite number' );
+
+    assert && assert( typeof power === 'number' && isFinite( power ) && power >= 0 && power <= 4 && power % 1 === 0,
+      'Power should be a finite integer between 0 and 4 (inclusive)' );
+
+    // @public {number}
+    this.coefficient = coefficient;
+
+    // @public {number}
+    this.power = power;
+  }
+
+  areaModelCommon.register( 'Term', Term );
+
+  return inherit( Object, Term );
+} );
