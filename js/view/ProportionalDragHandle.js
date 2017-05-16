d739d667a051153ed2aad514b7c319a4fdcc1114
Jonathan Olson <olsonsjc@gmail.com>
2017-05-16 12:59:55 -0600

    Smoothing for proportional drag handles, see https://github.com/phetsims/area-model-common/issues/3

--- js/view/ProportionalDragHandle.js
+++ js/view/ProportionalDragHandle.js
@@ -16,8 +16,10 @@ define( function( require ) {
   var Line = require( 'SCENERY/nodes/Line' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Orientation = require( 'AREA_MODEL_COMMON/model/Orientation' );
+  var Property = require( 'AXON/Property' );
   var SimpleDragHandler = require( 'SCENERY/input/SimpleDragHandler' );
   var Util = require( 'DOT/Util' );
+  var Vector2 = require( 'DOT/Vector2' );
 
   // constants
   var DRAG_OFFSET = 5;
@@ -35,40 +37,73 @@ define( function( require ) {
 
     var self = this;
 
-    Node.call( this, {
-      children: [
-        new Line( 0, 0, DRAG_OFFSET, DRAG_OFFSET, {
-          stroke: AreaModelColorProfile.proportionalDragHandleBorderProperty
-        } ),
-        new Circle( DRAG_RADIUS, {
-          x: CIRCLE_DRAG_OFFSET,
-          y: CIRCLE_DRAG_OFFSET,
-          fill: AreaModelColorProfile.proportionalDragHandleBackgroundProperty,
-          stroke: AreaModelColorProfile.proportionalDragHandleBorderProperty,
-          cursor: 'pointer',
-          inputListeners: [
-            // TODO: DragHandler? See https://github.com/phetsims/area-model-common/issues/17
-            new SimpleDragHandler( {
-              // TODO: key into starting drag point? See https://github.com/phetsims/area-model-common/issues/17
-              drag: function( event, trail ) {
-                var viewPoint = self.globalToParentPoint( event.pointer.point ).minusScalar( CIRCLE_DRAG_OFFSET );
-                var modelPoint = modelViewTransform.viewToModelPosition( viewPoint );
+    // {Property.<boolean>} - Whether this is being dragged (we only apply offsets when dragged)
+    var draggedProperty = new Property( false );
 
-                var width = Math.round( modelPoint.x / area.snapSize ) * area.snapSize;
-                var height = Math.round( modelPoint.y / area.snapSize ) * area.snapSize;
+    // {Property.<Vector2>} - The current view "offset" from where the pointer is compared to the point it is controlling
+    var offsetProperty = new Property( new Vector2() );
 
-                width = Util.clamp( width, area.minimumSize, area.maximumSize );
-                height = Util.clamp( height, area.minimumSize, area.maximumSize );
+    var line = new Line( {
+      stroke: AreaModelColorProfile.proportionalDragHandleBorderProperty
+    } );
 
-                area.getActiveTotalProperty( Orientation.HORIZONTAL ).value = width;
-                area.getActiveTotalProperty( Orientation.VERTICAL ).value = height;
-              }
-            } )
-          ]
+    var circle = new Circle( DRAG_RADIUS, {
+      fill: AreaModelColorProfile.proportionalDragHandleBackgroundProperty,
+      stroke: AreaModelColorProfile.proportionalDragHandleBorderProperty,
+      cursor: 'pointer',
+      inputListeners: [
+        // TODO: DragHandler? See https://github.com/phetsims/area-model-common/issues/17
+        new SimpleDragHandler( {
+          start: function( event, trail ) {
+            draggedProperty.value = true;
+          },
+          // TODO: key into starting drag point? See https://github.com/phetsims/area-model-common/issues/17
+          drag: function( event, trail ) {
+            var pointerViewPoint = self.globalToParentPoint( event.pointer.point );
+            var viewPoint = pointerViewPoint.minusScalar( CIRCLE_DRAG_OFFSET );
+            var modelPoint = modelViewTransform.viewToModelPosition( viewPoint );
+
+            var width = Math.round( modelPoint.x / area.snapSize ) * area.snapSize;
+            var height = Math.round( modelPoint.y / area.snapSize ) * area.snapSize;
+
+            width = Util.clamp( width, area.minimumSize, area.maximumSize );
+            height = Util.clamp( height, area.minimumSize, area.maximumSize );
+
+            area.getActiveTotalProperty( Orientation.HORIZONTAL ).value = width;
+            area.getActiveTotalProperty( Orientation.VERTICAL ).value = height;
+
+            offsetProperty.value = new Vector2(
+              viewPoint.x - modelViewTransform.modelToViewX( width ),
+              viewPoint.y - modelViewTransform.modelToViewY( height )
+            );
+          },
+          end: function( event, trail ) {
+            draggedProperty.value = false;
+          }
         } )
       ]
     } );
 
+    Node.call( this, {
+      children: [
+        line,
+        circle
+      ]
+    } );
+
+    // Apply offsets while dragging for a smoother experience.
+    // See https://github.com/phetsims/area-model-common/issues/3
+    Property.multilink( [ draggedProperty, offsetProperty ], function( dragged, offset ) {
+      var combinedOffset = 0;
+      if ( dragged ) {
+        // Project to the line y=x, and limit for when the user goes to 1x1 or the max.
+        combinedOffset = Util.clamp( ( offset.x + offset.y ) / 2, -10, 10 );
+      }
+      line.x2 = line.y2 = combinedOffset + DRAG_OFFSET;
+      circle.x = circle.y = combinedOffset + CIRCLE_DRAG_OFFSET;
+    } );
+
+    // Update the offset of the drag handle
     Orientation.VALUES.forEach( function( orientation ) {
       area.getActiveTotalProperty( orientation ).link( function( value ) {
         self[ orientation.coordinate ] = orientation.modelToView( modelViewTransform, value );
