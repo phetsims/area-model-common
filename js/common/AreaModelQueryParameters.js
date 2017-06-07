79178a2e75d2853984fe40c08a23900a876b5c36
Jonathan Olson <olsonsjc@gmail.com>
2017-06-07 11:58:17 -0600

    ComboBox alternative, see https://github.com/phetsims/area-model-common/issues/30

---
+++ js/common/AreaModelQueryParameters.js
@@ -0,0 +1,24 @@
+// Copyright 2017, University of Colorado Boulder
+
+/**
+ * Query parameters supported by area-model simulations.
+ *
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var areaModelCommon = require( 'AREA_MODEL_COMMON/areaModelCommon' );
+
+  var AreaModelQueryParameters = QueryStringMachine.getAll( {
+
+    // TODO: remove before deployment
+    // use a ComboBox for generic layout selection
+    combobox: { type: 'flag' }
+  } );
+
+  areaModelCommon.register( 'AreaModelQueryParameters', AreaModelQueryParameters );
+
+  return AreaModelQueryParameters;
+} );
--- js/generic/view/GenericLayoutSelectionNode.js
+++ js/generic/view/GenericLayoutSelectionNode.js
@@ -13,13 +13,21 @@ define( function( require ) {
   var AreaModelColorProfile = require( 'AREA_MODEL_COMMON/common/view/AreaModelColorProfile' );
   var areaModelCommon = require( 'AREA_MODEL_COMMON/areaModelCommon' );
   var AreaModelConstants = require( 'AREA_MODEL_COMMON/common/AreaModelConstants' );
+  var AreaModelQueryParameters = require( 'AREA_MODEL_COMMON/common/AreaModelQueryParameters' );
+  var BooleanProperty = require( 'AXON/BooleanProperty' );
   var ComboBox = require( 'SUN/ComboBox' );
+  var DerivedProperty = require( 'AXON/DerivedProperty' );
+  var FireListener = require( 'SCENERY/listeners/FireListener' );
   var GenericLayout = require( 'AREA_MODEL_COMMON/generic/model/GenericLayout' );
   var HBox = require( 'SCENERY/nodes/HBox' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var Line = require( 'SCENERY/nodes/Line' );
+  var Node = require( 'SCENERY/nodes/Node' );
   var Path = require( 'SCENERY/nodes/Path' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var Shape = require( 'KITE/Shape' );
   var Text = require( 'SCENERY/nodes/Text' );
+  var VBox = require( 'SCENERY/nodes/VBox' );
 
   /**
    * @constructor
@@ -30,13 +38,20 @@ define( function( require ) {
    * @param {number} width
    */
   function GenericLayoutSelectionNode( genericLayoutProperty, listParent, width ) {
+    Node.call( this );
 
+    var self = this;
 
+    width -= 1; //TODO: don't have to subtract off the double half stroke width here!
+
+    var scale = 0.7;
+
+    //TODO: refactor so this may not be needed if we ditch the ComboBox
     var items = GenericLayout.VALUES.map( function( layout ) {
       return {
         node: new HBox( {
           children: [
-            createLayoutIcon( layout.size ),
+            createLayoutIcon( layout.size, 1 ),
             new Text( layout.size.height + 'x' + layout.size.width, {
               font: AreaModelConstants.LAYOUT_FONT
             } )
@@ -47,19 +62,167 @@ define( function( require ) {
       };
     } );
 
-    // TODO: better way!
-    var currentComboChromeWidth = 74.5; // empirically determined, but easy to break in the future
-    var maxItemWidth = Math.max.apply( Math, _.map( _.map( items, 'node' ), 'width' ) );
-    var scale = 0.7;
-    var extraPadding = width / scale - currentComboChromeWidth - maxItemWidth;
-    items.forEach( function( item ) {
-      item.node = new AlignBox( item.node, { rightMargin: extraPadding } );
-    } );
+    if ( AreaModelQueryParameters.combobox ) {
+      // TODO: better way!
+      var currentComboChromeWidth = 74.5; // empirically determined, but easy to break in the future
+      var maxItemWidth = Math.max.apply( Math, _.map( _.map( items, 'node' ), 'width' ) );
+      var extraPadding = width / scale - currentComboChromeWidth - maxItemWidth;
+      items.forEach( function( item ) {
+        item.node = new AlignBox( item.node, { rightMargin: extraPadding } );
+      } );
 
-    ComboBox.call( this, items, genericLayoutProperty, listParent, {
-      // TODO
-      scale: scale
-    } );
+      this.addChild( new ComboBox( items, genericLayoutProperty, listParent, {
+        // TODO
+        scale: scale
+      } ) );
+    }
+    else {
+      var maxItemHeight = Math.max.apply( Math, _.map( _.map( items, 'node' ), 'height' ) );
+      var itemMargin = 6;
+      var arrowMargin = 8;
+
+      var rectHeight = maxItemHeight * scale + 2 * itemMargin;
+      var rectangle = new Rectangle( {
+        rectWidth: width,
+        rectHeight: maxItemHeight * scale + 2 * itemMargin,
+        fill: 'white',
+        stroke: 'black',
+        cornerRadius: AreaModelConstants.PANEL_CORNER_RADIUS,
+        cursor: 'pointer'
+      } );
+      this.addChild( rectangle );
+
+      var arrowSize = 15;
+      var arrow = new Path( new Shape().moveTo( 0, 0 ).lineTo( arrowSize, 0 ).lineTo( arrowSize * 0.5, arrowSize * 0.9 ).close(), {
+        fill: 'black',
+        right: rectangle.right - arrowMargin,
+        centerY: rectangle.centerY,
+        pickable: false
+      } );
+      this.addChild( arrow );
+
+      var separatorX = arrow.left - arrowMargin;
+      this.addChild( new Line( {
+        x1: separatorX,
+        y1: 0,
+        x2: separatorX,
+        y2: rectHeight,
+        lineWidth: 0.5,
+        stroke: 'black',
+        pickable: false
+      } ) );
+
+      var currentLabel = new Node( {
+        scale: scale, // TODO: get rid of scale when we don't need it for the ComboBox
+        pickable: false
+      } );
+      genericLayoutProperty.link( function( layout ) {
+        currentLabel.children = [
+          _.find( items, function( item ) {
+            return item.value === layout;
+          } ).node
+        ];
+        currentLabel.left = itemMargin;
+        currentLabel.centerY = rectangle.centerY;
+      } );
+      this.addChild( currentLabel );
+
+      var popup = new Rectangle( {
+        rectWidth: separatorX,
+        rectHeight: separatorX,
+        fill: 'white',
+        stroke: 'black',
+        cornerRadius: AreaModelConstants.PANEL_CORNER_RADIUS,
+        pickable: true
+      } );
+
+      var buttonSpacing = 6;
+      var buttonsNode = new VBox( {
+        children: [ 1, 2, 3 ].map( function( numHorizontal ) {
+          return new HBox( {
+            children: [ 1, 2, 3 ].map( function( numVertical ) {
+              var layout = GenericLayout.fromValues( numHorizontal, numVertical );
+              var icon = createLayoutIcon( layout.size, 0.7 );
+              icon.pickable = false; // TODO: annoying that we have to specify this?
+              var cornerRadius = 3;
+              var background = Rectangle.roundedBounds( icon.bounds.dilated( cornerRadius ), cornerRadius, cornerRadius, {
+                cursor: 'pointer'
+              } );
+              var listener = new FireListener( {
+                fire: function() {
+                  genericLayoutProperty.value = layout;
+                  visibleProperty.value = false; // hide
+                }
+              } );
+              background.stroke = new DerivedProperty( [ genericLayoutProperty, AreaModelColorProfile.radioBorderProperty ], function( currentLayout, highlightColor ) {
+                if ( currentLayout === layout ) {
+                  return highlightColor;
+                }
+                else {
+                  return 'transparent';
+                }
+              } );
+              background.fill = new DerivedProperty( [ listener.isHoveringProperty ], function( isHovering ) {
+                if ( isHovering ) {
+                  return 'rgb(240,240,240)'; // TODO: extract
+                }
+                else {
+                  return 'transparent';
+                }
+              } );
+
+              return new Node( {
+                children: [ background, icon ],
+                inputListeners: [ listener ]
+              } );
+            } ),
+            spacing: buttonSpacing
+          } );
+        } ),
+        spacing: buttonSpacing
+      } );
+      var panelMargin = 20;
+      buttonsNode.scale( ( popup.width - 2 * panelMargin ) / buttonsNode.width );
+      buttonsNode.center = popup.center;
+      popup.addChild( buttonsNode );
+
+      var visibleProperty = new BooleanProperty( false );
+      popup.addInputListener( {
+        down: function( event ) {
+          event.handle();
+        }
+      } );
+      //TODO: input cleanup! Messy. Bad! Make Input Listeners Great Again!
+      var dismissListener = {
+        down: function( event ) {
+          if ( !event.trail.isExtensionOf( self.getUniqueTrail() ) ) {
+            visibleProperty.value = false;
+          }
+        }
+      };
+      visibleProperty.lazyLink( function( visible ) {
+        if ( visible ) {
+          var matrix = self.getUniqueTrail().getMatrixTo( listParent.getUniqueTrail() );
+          popup.setScaleMagnitude( matrix.getScaleVector().x );
+          // TODO: handle scale sometime maybe?
+          popup.leftTop = matrix.timesVector2( rectangle.leftBottom );
+          listParent.addChild( popup );
+
+          self.getUniqueTrail().rootNode().addInputListener( dismissListener );
+        }
+        else {
+          listParent.removeChild( popup );
+
+          self.getUniqueTrail().rootNode().removeInputListener( dismissListener );
+        }
+      } );
+
+      rectangle.addInputListener( {
+        down: function( event ) {
+          visibleProperty.toggle();
+        }
+      } );
+    }
   }
 
   areaModelCommon.register( 'GenericLayoutSelectionNode', GenericLayoutSelectionNode );
@@ -69,9 +232,10 @@ define( function( require ) {
    * @private
    *
    * @param {Dimension2} size
+   * @param {number} lineWidth
    * @returns {Node}
    */
-  function createLayoutIcon( size ) {
+  function createLayoutIcon( size, lineWidth ) {
     var length = 30;
     var shape = new Shape().rect( 0, 0, length, length );
     if ( size.width === 2 ) {
@@ -89,9 +253,11 @@ define( function( require ) {
       shape.moveTo( 0, length * AreaModelConstants.GENERIC_ICON_SECOND_OFFSET ).horizontalLineTo( length );
     }
     return new Path( shape, {
-      stroke: AreaModelColorProfile.layoutGridProperty
+      lineWidth: lineWidth,
+      stroke: AreaModelColorProfile.layoutGridProperty,
+      fill: 'white' //TODO colorit
     } );
   }
 
-  return inherit( ComboBox, GenericLayoutSelectionNode );
+  return inherit( Node, GenericLayoutSelectionNode );
 } );
