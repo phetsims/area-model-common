06674aa63de30655a270016f039ba971017c148f
Jonathan Olson <jonathan.olson@colorado.edu>
2018-03-14 00:27:04 -0600

    createPartitionOrientationIcon cleanup

--- js/proportional/view/PartitionSelectionNode.js
+++ js/proportional/view/PartitionSelectionNode.js
@@ -17,6 +17,7 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var Line = require( 'SCENERY/nodes/Line' );
   var Orientation = require( 'AREA_MODEL_COMMON/common/model/Orientation' );
+  var OrientationPair = require( 'AREA_MODEL_COMMON/common/model/OrientationPair' );
   var Path = require( 'SCENERY/nodes/Path' );
   var ProportionalPartitionLineNode = require( 'AREA_MODEL_COMMON/proportional/view/ProportionalPartitionLineNode' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
@@ -54,38 +55,35 @@ define( function( require ) {
      * @returns {Node}
      */
     createPartitionOrientationIcon: function( orientation, currentAreaOrientationProperty ) {
-      // TODO: cleanup
-      var sizes = {
-        x: 30 * 1.2,
-        y: 20 * 1.2
-      };
-      var background = new Rectangle( 0, 0, sizes.x, sizes.y, {
+      // The size of our rectangle
+      var sizes = new OrientationPair( 36, 24 );
+      var background = new Rectangle( 0, 0, sizes.horizontal, sizes.vertical, {
         stroke: AreaModelCommonColorProfile.partitionLineIconBorderProperty,
         fill: AreaModelCommonColorProfile.partitionLineIconBackgroundProperty
       } );
+
+      // Expand bounds a bit, to allow room for the line-handle icon part (so we have even padding)
       background.localBounds = background.localBounds.dilated( 7 );
 
       var p1 = new Vector2();
       var p2 = new Vector2();
+      p1[ orientation.coordinate ] = sizes.get( orientation ) * 2 / 3;
+      p2[ orientation.coordinate ] = sizes.get( orientation ) * 2 / 3;
+      p2[ orientation.opposite.coordinate ] = sizes.get( orientation.opposite ) * 1.1;
 
-      p1[ orientation.coordinate ] = sizes[ orientation.coordinate ] * 2 / 3;
-      p2[ orientation.coordinate ] = sizes[ orientation.coordinate ] * 2 / 3;
-      p2[ orientation.opposite.coordinate ] = sizes[ orientation.opposite.coordinate ] * 1.1;
-
-      background.addChild( new Line( p1, p2, {
-        stroke: AreaModelCommonColorProfile.partitionLineIconLineProperty
-      } ) );
-
-      var handleShape = ProportionalPartitionLineNode.HANDLE_ARROW_SHAPES.get( orientation );
-
-      background.addChild( new Path( handleShape, {
-        fill: new DerivedProperty( [ currentAreaOrientationProperty, AreaModelCommonColorProfile.proportionalColorProperties.get( orientation ) ], function( currentOrientation, widthColor ) {
-          return currentOrientation === orientation ? widthColor : '#333';
+      background.children = [
+        new Line( p1, p2, {
+          stroke: AreaModelCommonColorProfile.partitionLineIconLineProperty
         } ),
-        scale: 0.5,
-        translation: p2
-      } ) );
-
+        new Path( ProportionalPartitionLineNode.HANDLE_ARROW_SHAPES.get( orientation ), {
+          fill: new DerivedProperty( [ currentAreaOrientationProperty, AreaModelCommonColorProfile.proportionalColorProperties.get( orientation ) ], function( currentOrientation, widthColor ) {
+            return currentOrientation === orientation ? widthColor : '#333';
+          } ),
+          scale: 0.5,
+          translation: p2
+        } )
+      ];
+      
       return background;
     }
   } );
