4803de65ccd3baad4947b64ce8c4c5f5797a2761
Jonathan Olson <jonathan.olson@colorado.edu>
2018-04-14 00:23:54 -0600

    Hardcoding 123 string for https://github.com/phetsims/area-model-common/issues/104

--- js/proportional/view/ProportionalAreaScreenView.js
+++ js/proportional/view/ProportionalAreaScreenView.js
@@ -35,7 +35,6 @@ define( function( require ) {
   var VBox = require( 'SCENERY/nodes/VBox' );
 
   // strings
-  var countingToggleString = require( 'string!AREA_MODEL_COMMON/countingToggle' );
   var partitionString = require( 'string!AREA_MODEL_COMMON/partition' );
 
   // constants
@@ -255,10 +254,9 @@ define( function( require ) {
      * @returns {Node}
      */
     createCountingIconNode: function() {
-      // REVIEW: Should this have a max width for i18n. StringTest=long seems to overlap other panels.
-      return new Text( countingToggleString, {
-        font: AreaModelCommonConstants.COUNTING_ICON_FONT,
-        maxWidth: 60
+      // Hardcoded string, see https://github.com/phetsims/area-model-common/issues/104
+      return new Text( '123', {
+        font: AreaModelCommonConstants.COUNTING_ICON_FONT
       } );
     }
   } );
