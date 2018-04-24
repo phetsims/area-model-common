5b458328034782eb5b2ec15acaed2120bee5264f
Jonathan Olson <jonathan.olson@colorado.edu>
2018-04-24 10:01:24 -0600

    Fixed unit tests, see #99

--- js/common/model/TermTests.js
+++ js/common/model/TermTests.js
@@ -20,8 +20,8 @@ define( function( require ) {
   } );
 
   QUnit.test( 'Multiplication', function( assert ) {
-    assert.ok( new Term( 3, 0 ).times( new Term( 5, 0 ).equals( new Term( 15, 0 ) ) ), 'Basic multiplication' );
-    assert.ok( new Term( 3, 1 ).times( new Term( 5, 0 ).equals( new Term( 15, 1 ) ) ), 'Basic multiplication (one exponent)' );
-    assert.ok( new Term( 3, 2 ).times( new Term( 5, 1 ).equals( new Term( 15, 3 ) ) ), 'Basic multiplication (two exponents)' );
+    assert.ok( new Term( 3, 0 ).times( new Term( 5, 0 ) ).equals( new Term( 15, 0 ) ), 'Basic multiplication' );
+    assert.ok( new Term( 3, 1 ).times( new Term( 5, 0 ) ).equals( new Term( 15, 1 ) ), 'Basic multiplication (one exponent)' );
+    assert.ok( new Term( 3, 2 ).times( new Term( 5, 1 ) ).equals( new Term( 15, 3 ) ), 'Basic multiplication (two exponents)' );
   } );
 } );
