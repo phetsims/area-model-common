9ceb56c3af53057bebf1050c7e013ec5f8cf11e7
pixelzoom <cmalley@pixelzoom.com>
2020-09-29 00:34:32 -0600

    convert AccordionBox subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/proportional/view/CalculationBox.js
+++ js/proportional/view/CalculationBox.js
@@ -9,10 +9,9 @@
  */
 
 import Bounds2 from '../../../../dot/js/Bounds2.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import AlignBox from '../../../../scenery/js/nodes/AlignBox.js';
-import areaModelCommonStrings from '../../areaModelCommonStrings.js';
 import areaModelCommon from '../../areaModelCommon.js';
+import areaModelCommonStrings from '../../areaModelCommonStrings.js';
 import AreaCalculationChoice from '../../common/model/AreaCalculationChoice.js';
 import AreaModelCommonAccordionBox from '../../common/view/AreaModelCommonAccordionBox.js';
 import CalculationLinesNode from '../../common/view/calculation/CalculationLinesNode.js';
@@ -24,76 +23,70 @@ const calculationBoxDescriptionString = areaModelCommonStrings.a11y.calculationB
 // constants
 const MARGIN = 8;
 
-/**
- * @constructor
- * @extends {AreaModelCommonAccordionBox}
- *
- * @param {ProportionalAreaModel} model
- * @param {Bounds2} bounds - Where to lay out the box in view space
- * @param {Object} [nodeOptions]
- */
-function CalculationBox( model, bounds, nodeOptions ) {
+class CalculationBox extends AreaModelCommonAccordionBox {
 
-  const self = this;
+  /**
+   * @param {ProportionalAreaModel} model
+   * @param {Bounds2} bounds - Where to lay out the box in view space
+   * @param {Object} [nodeOptions]
+   */
+  constructor( model, bounds, nodeOptions ) {
 
-  // @private {ProportionalAreaModel}
-  this.model = model;
+    const calculationLinesNode = new CalculationLinesNode( model );
 
-  // @private {CalculationLinesNode}
-  this.calculationLinesNode = new CalculationLinesNode( model );
+    const alignBox = new AlignBox( calculationLinesNode, {
+      // Since our AccordionBox expands by our MARGIN, we need to set content bounds without that
+      alignBounds: bounds.eroded( MARGIN ),
+      pickable: false
+    } );
 
-  const alignBox = new AlignBox( this.calculationLinesNode, {
-    // Since our AccordionBox expands by our MARGIN, we need to set content bounds without that
-    alignBounds: bounds.eroded( MARGIN ),
-    pickable: false
-  } );
+    super( calculationString, model.calculationBoxVisibleProperty, alignBox, {
+      // Different margins than our other accordion boxes
+      contentXMargin: MARGIN,
+      contentYMargin: MARGIN,
 
-  AreaModelCommonAccordionBox.call( this, calculationString, model.calculationBoxVisibleProperty, alignBox, {
-    // Different margins than our other accordion boxes
-    contentXMargin: MARGIN,
-    contentYMargin: MARGIN,
+      // Different spacing
+      maxTitleWidth: 290,
 
-    // Different spacing
-    maxTitleWidth: 290,
+      // We don't have room for the normal spacing, so we need to make things closer together than they normally are.
+      contentXSpacing: -10,
 
-    // We don't have room for the normal spacing, so we need to make things closer together than they normally are.
-    contentXSpacing: -10,
+      // pdom
+      labelTagName: 'h3',
+      labelContent: calculationBoxTitleString,
+      titleBarOptions: {
+        descriptionContent: calculationBoxDescriptionString
+      }
+    } );
 
-    // pdom
-    labelTagName: 'h3',
-    labelContent: calculationBoxTitleString,
-    titleBarOptions: {
-      descriptionContent: calculationBoxDescriptionString
-    }
-  } );
+    model.areaCalculationChoiceProperty.link( choice => {
+      assert && assert( choice !== AreaCalculationChoice.LINE_BY_LINE, 'Should be HIDDEN or SHOW_ALL_LINES' );
+      this.visible = choice !== AreaCalculationChoice.HIDDEN;
+    } );
 
-  model.areaCalculationChoiceProperty.link( function( choice ) {
-    assert && assert( choice !== AreaCalculationChoice.LINE_BY_LINE, 'Should be HIDDEN or SHOW_ALL_LINES' );
-    self.visible = choice !== AreaCalculationChoice.HIDDEN;
-  } );
+    // Resize things so our AccordionBox is the correct size (we can't get bounds correct initially, because of the
+    // expand button shifting content)
+    alignBox.alignBounds = new Bounds2(
+      0,
+      0,
+      alignBox.alignBounds.width - ( this.width - bounds.width ),
+      alignBox.alignBounds.height - ( this.height - bounds.height )
+    );
 
-  // Resize things so our AccordionBox is the correct size (we can't get bounds correct initially, because of the
-  // expand button shifting content)
-  alignBox.alignBounds = new Bounds2(
-    0,
-    0,
-    alignBox.alignBounds.width - ( this.width - bounds.width ),
-    alignBox.alignBounds.height - ( this.height - bounds.height )
-  );
+    this.mutate( nodeOptions );
 
-  this.mutate( nodeOptions );
-}
+    // @private
+    this.calculationLinesNode = calculationLinesNode;
+  }
 
-areaModelCommon.register( 'CalculationBox', CalculationBox );
-
-inherit( AreaModelCommonAccordionBox, CalculationBox, {
   /**
    * Updates the content of the calculation box (if needed).
    * @public
    */
-  update: function() {
+  update() {
     this.calculationLinesNode.update();
   }
-} );
+}
 
+areaModelCommon.register( 'CalculationBox', CalculationBox );
 export default CalculationBox;
