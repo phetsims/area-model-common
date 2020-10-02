bb0e97eb6cf07d29e58c8202e3bbf4a72f616572
pixelzoom <cmalley@pixelzoom.com>
2020-10-01 22:05:23 -0600

    convert Panel subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/generic/view/TermKeypadPanel.js
+++ js/generic/view/TermKeypadPanel.js
@@ -8,7 +8,6 @@
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import BackspaceIcon from '../../../../scenery-phet/js/BackspaceIcon.js';
 import Key from '../../../../scenery-phet/js/keypad/Key.js';
 import KeyID from '../../../../scenery-phet/js/keypad/KeyID.js';
@@ -21,8 +20,8 @@ import Text from '../../../../scenery/js/nodes/Text.js';
 import VBox from '../../../../scenery/js/nodes/VBox.js';
 import RectangularPushButton from '../../../../sun/js/buttons/RectangularPushButton.js';
 import Panel from '../../../../sun/js/Panel.js';
-import areaModelCommonStrings from '../../areaModelCommonStrings.js';
 import areaModelCommon from '../../areaModelCommon.js';
+import areaModelCommonStrings from '../../areaModelCommonStrings.js';
 import AreaModelCommonConstants from '../../common/AreaModelCommonConstants.js';
 import Term from '../../common/model/Term.js';
 import AreaModelCommonColorProfile from '../../common/view/AreaModelCommonColorProfile.js';
@@ -66,113 +65,112 @@ const exponentLayout = noExponentLayout.concat( [
   ]
 ] );
 
-/**
- * @constructor
- * @extends {Panel}
- *
- * @param {Property.<number>} digitCountProperty
- * @param {boolean} allowExponents - Whether exponents (powers of x) are allowed
- * @param {boolean} allowNegative
- * @param {function} enterCallback - function( {Term|null} ) - The entered term, or null if there is no valid term entered.
- * @param {Object} [nodeOptions]
- */
-function TermKeypadPanel( digitCountProperty, allowExponents, allowNegative, enterCallback, nodeOptions ) {
-  assert && assert( allowNegative || !allowExponents, 'We have no non-negative exponent keyboard layout' );
+class TermKeypadPanel extends Panel {
 
-  // Handles logic for key-presses and conversion to strings/Terms.
-  const termAccumulator = new TermAccumulator( digitCountProperty );
+  /**
+   * @param {Property.<number>} digitCountProperty
+   * @param {boolean} allowExponents - Whether exponents (powers of x) are allowed
+   * @param {boolean} allowNegative
+   * @param {function} enterCallback - function( {Term|null} ) - The entered term, or null if there is no valid term entered.
+   * @param {Object} [nodeOptions]
+   */
+  constructor( digitCountProperty, allowExponents, allowNegative, enterCallback, nodeOptions ) {
+    assert && assert( allowNegative || !allowExponents, 'We have no non-negative exponent keyboard layout' );
 
-  // @private {Keypad}
-  this.keypad = new Keypad( allowExponents ? exponentLayout : ( allowNegative ? noExponentLayout : noNegativeLayout ), {
-    accumulator: termAccumulator
-  } );
+    // Handles logic for key-presses and conversion to strings/Terms.
+    const termAccumulator = new TermAccumulator( digitCountProperty );
 
-  const readoutBackground = new Rectangle( {
-    fill: AreaModelCommonColorProfile.keypadReadoutBackgroundProperty,
-    stroke: AreaModelCommonColorProfile.keypadReadoutBorderProperty,
-    cornerRadius: AreaModelCommonConstants.PANEL_CORNER_RADIUS
-  } );
+    const keypad = new Keypad( allowExponents ? exponentLayout : ( allowNegative ? noExponentLayout : noNegativeLayout ), {
+      accumulator: termAccumulator
+    } );
 
-  const readoutTextOptions = {
-    font: AreaModelCommonConstants.KEYPAD_READOUT_FONT
-  };
+    const readoutBackground = new Rectangle( {
+      fill: AreaModelCommonColorProfile.keypadReadoutBackgroundProperty,
+      stroke: AreaModelCommonColorProfile.keypadReadoutBorderProperty,
+      cornerRadius: AreaModelCommonConstants.PANEL_CORNER_RADIUS
+    } );
 
-  const readoutText = new RichText( '', readoutTextOptions );
+    const readoutTextOptions = {
+      font: AreaModelCommonConstants.KEYPAD_READOUT_FONT
+    };
 
-  function updateText( string ) {
-    // Trick to be able to position an empty string
-    readoutText.visible = string.length > 0;
-    if ( readoutText.visible ) {
-      readoutText.text = string;
-      readoutText.centerX = readoutBackground.centerX;
+    const readoutText = new RichText( '', readoutTextOptions );
+
+    function updateText( string ) {
+      // Trick to be able to position an empty string
+      readoutText.visible = string.length > 0;
+      if ( readoutText.visible ) {
+        readoutText.text = string;
+        readoutText.centerX = readoutBackground.centerX;
+      }
     }
+
+    // Update the text when the accumulator's string output changes
+    termAccumulator.richStringProperty.link( updateText );
+
+    // When the active partition changes, resize the background to fit to the largest size.
+    digitCountProperty.link( function( digitCount ) {
+      // Temporarily use a different string
+      readoutText.text = Term.getLargestGenericString( allowExponents, digitCount );
+
+      // Update the background
+      readoutBackground.setRectBounds( readoutText.bounds.dilatedXY( 10, 1 ) );
+
+      // Reposition our text
+      readoutText.center = readoutBackground.center;
+
+      // Reset the text value back to what it should be.
+      updateText( termAccumulator.richStringProperty.value );
+    } );
+
+    super( new VBox( {
+      children: [
+        new Node( {
+          // We position the text over the background manually
+          children: [
+            readoutBackground,
+            readoutText
+          ]
+        } ),
+        keypad,
+        new RectangularPushButton( {
+          content: new Text( enterString, {
+            font: AreaModelCommonConstants.KEYPAD_FONT,
+            maxWidth: 100
+          } ),
+          touchAreaXDilation: 5,
+          touchAreaYDilation: 5,
+          xMargin: 15,
+          yMargin: 5,
+          listener: function() {
+            enterCallback( termAccumulator.termProperty.value );
+          },
+          baseColor: AreaModelCommonColorProfile.keypadEnterBackgroundProperty
+        } )
+      ],
+      spacing: 10
+    } ), {
+      cornerRadius: AreaModelCommonConstants.PANEL_CORNER_RADIUS,
+      xMargin: 15,
+      yMargin: 15,
+      fill: AreaModelCommonColorProfile.keypadPanelBackgroundProperty,
+      stroke: AreaModelCommonColorProfile.keypadPanelBorderProperty
+    } );
+
+    this.mutate( nodeOptions );
+
+    // @private
+    this.keypad = keypad;
   }
 
-  // Update the text when the accumulator's string output changes
-  termAccumulator.richStringProperty.link( updateText );
-
-  // When the active partition changes, resize the background to fit to the largest size.
-  digitCountProperty.link( function( digitCount ) {
-    // Temporarily use a different string
-    readoutText.text = Term.getLargestGenericString( allowExponents, digitCount );
-
-    // Update the background
-    readoutBackground.setRectBounds( readoutText.bounds.dilatedXY( 10, 1 ) );
-
-    // Reposition our text
-    readoutText.center = readoutBackground.center;
-
-    // Reset the text value back to what it should be.
-    updateText( termAccumulator.richStringProperty.value );
-  } );
-
-  Panel.call( this, new VBox( {
-    children: [
-      new Node( {
-        // We position the text over the background manually
-        children: [
-          readoutBackground,
-          readoutText
-        ]
-      } ),
-      this.keypad,
-      new RectangularPushButton( {
-        content: new Text( enterString, {
-          font: AreaModelCommonConstants.KEYPAD_FONT,
-          maxWidth: 100
-        } ),
-        touchAreaXDilation: 5,
-        touchAreaYDilation: 5,
-        xMargin: 15,
-        yMargin: 5,
-        listener: function() {
-          enterCallback( termAccumulator.termProperty.value );
-        },
-        baseColor: AreaModelCommonColorProfile.keypadEnterBackgroundProperty
-      } )
-    ],
-    spacing: 10
-  } ), {
-    cornerRadius: AreaModelCommonConstants.PANEL_CORNER_RADIUS,
-    xMargin: 15,
-    yMargin: 15,
-    fill: AreaModelCommonColorProfile.keypadPanelBackgroundProperty,
-    stroke: AreaModelCommonColorProfile.keypadPanelBorderProperty
-  } );
-
-  this.mutate( nodeOptions );
-}
-
-areaModelCommon.register( 'TermKeypadPanel', TermKeypadPanel );
-
-inherit( Panel, TermKeypadPanel, {
   /**
    * Clears the keypad's content.
    * @public
    */
-  clear: function() {
+  clear() {
     this.keypad.clear();
   }
-} );
+}
 
+areaModelCommon.register( 'TermKeypadPanel', TermKeypadPanel );
 export default TermKeypadPanel;
