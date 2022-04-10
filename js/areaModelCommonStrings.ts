28087ebe49e7e16ff5a6dd9b10fdcc41ef1e3e61
Sam Reid <reids@colorado.edu>
2022-04-09 22:17:09 -0600

    Rename image files *.js => *.ts, see https://github.com/phetsims/chipper/issues/1218

--- js/areaModelCommonStrings.ts
+++ js/areaModelCommonStrings.ts
@@ -3,11 +3,99 @@
 /**
  * Auto-generated from modulify, DO NOT manually modify.
  */
-
+/* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
 import areaModelCommon from './areaModelCommon.js';
 
-const areaModelCommonStrings = getStringModule( 'AREA_MODEL_COMMON' );
+type StringsType = {
+  'screen': {
+    'explore': string,
+    'generic': string,
+    'variables': string,
+    'multiply': string,
+    'partition': string,
+    'decimals': string,
+    'game': string
+  },
+  'dimensions': string,
+  'factors': string,
+  'totalAreaOfModel': string,
+  'product': string,
+  'partition': string,
+  'areaModelCalculation': string,
+  'calculation': string,
+  'partialProducts': string,
+  'enter': string,
+  'area': string,
+  'scorePrefix': string,
+  'levelPrompt': {
+    'totalArea': string,
+    'oneProduct': {
+      'text': string,
+      'totalArea': string,
+      'oneLength': string
+    },
+    'twoProducts': string,
+    'twoLengths': string,
+    'threeLengths': string
+  },
+  'a11y': {
+    'areaEqualsPattern': string,
+    'areaGrid': string,
+    'areaGridRectanglePattern': string,
+    'areaGridSize': string,
+    'base10AreaTiles': string,
+    'betweenCalculationLines': string,
+    'calculationBoxTitle': string,
+    'calculationBoxDescription': string,
+    'countingNumbersDescription': string,
+    'countingNumbersLabel': string,
+    'countingNumbersPattern': string,
+    'dragHandle': string,
+    'dragHandleDescriptionPattern': string,
+    'erase': string,
+    'eraseDescription': string,
+    'factorsBox': string,
+    'factorsBoxDescription': string,
+    'factorsTimesPattern': string,
+    'gridLinesLabel': string,
+    'hidePartialProducts': string,
+    'horizontalDimensionCapitalized': string,
+    'horizontalPartition': string,
+    'horizontalPartitionHandle': string,
+    'horizontalPartitionHandleDescription': string,
+    'horizontalPicker': string,
+    'horizontalPickerDescription': string,
+    'multiplyDescription': string,
+    'onePartialProductFactorPattern': string,
+    'onePartialProductPattern': string,
+    'partitionDescription': string,
+    'partitionSelectionDescription': string,
+    'placeholder': string,
+    'productBox': string,
+    'productBoxDescription': string,
+    'productTimesPattern': string,
+    'quantityPattern': string,
+    'questionMark': string,
+    'sceneSelectionPattern': string,
+    'showPartialProductFactors': string,
+    'showPartialProducts': string,
+    'sumMinus': string,
+    'sumPlus': string,
+    'threePartitionsSplitPattern': string,
+    'twoPartialProductFactorsPattern': string,
+    'twoPartialProductsPattern': string,
+    'twoPartitionsSplitPattern': string,
+    'verticalDimensionCapitalized': string,
+    'verticalPartition': string,
+    'verticalPartitionHandle': string,
+    'verticalPartitionHandleDescription': string,
+    'verticalPicker': string,
+    'verticalPickerDescription': string
+  }
+};
+
+const areaModelCommonStrings = getStringModule( 'AREA_MODEL_COMMON' ) as StringsType;
 
 areaModelCommon.register( 'areaModelCommonStrings', areaModelCommonStrings );
 
