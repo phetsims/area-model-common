5ac038bc41b0e96c8374c62a0ba3b4e53ce365e4
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-03-22 14:45:12 -0600

    move usages to scenery-phet Rich drag listeners, https://github.com/phetsims/scenery/issues/1592
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- js/proportional/view/ProportionalPartitionLineNode.js
+++ js/proportional/view/ProportionalPartitionLineNode.js
@@ -22,7 +22,7 @@ import OrientationPair from '../../../../phet-core/js/OrientationPair.js';
 import ArrowNode from '../../../../scenery-phet/js/ArrowNode.js';
 import { HighlightPath, Line, Node, Path } from '../../../../scenery/js/imports.js';
 import AccessibleSlider from '../../../../sun/js/accessibility/AccessibleSlider.js';
-import RichDragListener from '../../../../sun/js/RichDragListener.js';
+import RichDragListener from '../../../../scenery-phet/js/RichDragListener.js';
 import ValueChangeSoundPlayer from '../../../../tambo/js/sound-generators/ValueChangeSoundPlayer.js';
 import areaModelCommon from '../../areaModelCommon.js';
 import AreaModelCommonStrings from '../../AreaModelCommonStrings.js';
